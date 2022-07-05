42aedd79a7b23dbcee9a3a9f63ca90097dec59c0
momia15 <GabiMaidanaxD@hotmail.com>
2022-07-05 20:19:49 -0300

    Colors [Navigation]

---
+++ src/navigation/PlaceNavigator.js
@@ -0,0 +1,3 @@
+import React from "react";
+import { Platform } from "react-native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
---
+++ src/utils/colors.js
@@ -0,0 +1,10 @@
+export default {
+    one: '#3e2c4e',
+    two: '#603e68',
+    three: '#94588e',
+    four: '#dd7dbe',
+    five: '#ffadf9',
+    white: '#FFFFFF',
+    black: '#000000',
+    gray: '#78767c',
+};

05f40735eb72102a9e1723bb698b5a8d5ec8c4d8
momia15 <GabiMaidanaxD@hotmail.com>
2022-07-06 19:32:47 -0300

    Create folder structure [ Navigation & Redux ]

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
 import AppNavigator from './navigation/index';
+import { Provider } from 'react-redux';
+import { store } from './store';
 
 export default function Route() {
-  return <AppNavigator/>
+  return (
+    <Provider store={store}>
+      <AppNavigator/>
+    </Provider>
+  )
 }
